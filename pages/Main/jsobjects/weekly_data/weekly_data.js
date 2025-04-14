0a121ad2fe1682dba2a59844705705fd6b959c04
Thanh Truong <thanhacun@gmail.com>
2025-04-14 06:18:23 +0000

    Typos

--- pages/Main/jsobjects/weekly_data/weekly_data.js
+++ pages/Main/jsobjects/weekly_data/weekly_data.js
@@ -1,12 +1,4 @@
 export default {
-	myFun1 () {
-		//	write code here
-		//	this.myVar1 = [1,2,3]
-	},
-	async myFun2 () {
-		//	use async-await or promises
-		//	await storeValue('varName', 'hello world')
-	},
 	daily_data: GetWorkers.data.list,
 	
 	draft_weekly_data: this.daily_data.map(e=>({
