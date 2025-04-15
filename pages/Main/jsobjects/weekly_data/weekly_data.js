e5ad524d7134cc6e1b5a6e41d718cee25f77f7eb
Thanh Truong <thanhacun@gmail.com>
2025-04-15 06:16:04 +0000

    Typos

--- pages/Main/jsobjects/weekly_data/weekly_data.js
+++ pages/Main/jsobjects/weekly_data/weekly_data.js
@@ -1,10 +1,11 @@
 export default {
-	daily_data: GetWorkers.data.list,
-	
+	get_data: () => GetWorkers.data.list,
+	daily_data: this.get_data(),
+
 	draft_weekly_data: this.daily_data.map(e=>({
 		week: e.Year + "-" + e.Month + "-W" + e.Week,
 		workers: this.daily_data.filter(d => (d.Year === e.Year && d.Month === e.Month && d.Week === e.Week)).map(e1 => e1.Workers).reduce( (a, c) => (a + c))
 	})),
-	
+
 	weekly_data: this.draft_weekly_data.filter( (e, index, self) => index === self.findIndex((t) => t.week === e.week))
 }
